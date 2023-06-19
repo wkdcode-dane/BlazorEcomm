14783887fb4045a490a7fb51ec1042c4e4a9f6dc
wkdcode-dane <dane.bridgman@wkdcode.co.uk>
2023-06-19 12:52:36 +0100

    Initial Commit / Making API

---
+++ BlazorEcomm/BlazorEcomm/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorEcomm.Client.Shared
+{
+    using BlazorEcomm.Shared;
+    using Microsoft.AspNetCore.Components;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Net.Http.Json;
+    using System.Threading.Tasks;
+
+    public partial class ProductList
+    {
+        [Inject]
+        HttpClient Http { get; set; } = new HttpClient();
+
+        private static List<Product> Products = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var result = await Http.GetFromJsonAsync<List<Product>>("api/products");
+
+            if (result != null)
+                Products = result;
+        }
+    }
+}
