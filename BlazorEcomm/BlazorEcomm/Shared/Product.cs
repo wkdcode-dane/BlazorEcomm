8a348a7ce53f247b48516d57a55e9c327b00ba76
wkdcode-dane <dane.bridgman@wkdcode.co.uk>
2023-06-28 15:30:46 +0100

    Adding categories and producing migrations and seeders

--- BlazorEcomm/BlazorEcomm/Shared/Product.cs
+++ BlazorEcomm/BlazorEcomm/Shared/Product.cs
@@ -15,5 +15,9 @@ namespace BlazorEcomm.Shared
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
+        public Category? Category { get; set; }
+
+        public int CategoryId { get; set; }
+
     }
 }
